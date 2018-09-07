1f196add32dacd59e034e980d083ec98bbc5c634
Julia Ballas <julia.m.ballas@gmail.com>
2018-09-07 09:06:09 -0600

    hw-3 folder added
    Created new folder for Week 3 homework with blank sketch.js, and wrote up a simple program to say 'Hello World'.

---
+++ hw-3/sketch.js
@@ -0,0 +1,16 @@
+//Write and declare the setup function
+function setup(){
+
+    //create canvas 600 px wide x 400 px high
+    createCanvas(600, 400);
+
+    //color background pink
+    background('pink');
+}
+//write and declare a draw function
+function draw(){
+
+    //write a statement to print the text string 'Hello World'
+    text('Hello World!', 20, 30);
+
+}
