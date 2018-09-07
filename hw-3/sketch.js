7e3ac13f3c447f449c2e784836efacfbcfc8b85d
Julia Ballas <julia.m.ballas@gmail.com>
2018-09-07 09:29:01 -0600

    hw-3 customize size,color,posistion
    I changed the canvas size, the background color to lilac and the posistion of the text.

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,16 +1,18 @@
 //Write and declare the setup function
 function setup(){
 
-    //create canvas 600 px wide x 400 px high
-    createCanvas(600, 400);
+    //create canvas 250 px wide x 250 px high
+    createCanvas(200, 200);
 
-    //color background pink
-    background('pink');
+    //color background changed to hexadecimal, I wanted it to be lilac.
+    background('#C8A2C8');
 }
+
 //write and declare a draw function
 function draw(){
 
     //write a statement to print the text string 'Hello World'
-    text('Hello World!', 20, 30);
+    //set the location parameters of x=75 and y=75; otherwise with the default settings the text does not show up on our canvas. 
+    text('Hello World!', 75, 75);
 
 }
