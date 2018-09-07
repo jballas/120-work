43ac6e40ad047bf10b27287adebfc12cac1c9100
Julia Ballas <julia.m.ballas@gmail.com>
2018-09-07 09:47:47 -0600

    hw-3 changed font style and size
    I adjusted the font style and size so it was easier to read.

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -6,6 +6,10 @@ function setup(){
 
     //color background changed to hexadecimal, I wanted it to be lilac.
     background('#C8A2C8');
+
+    //changed font style and size so it would be easier to read.
+    textFont('Georgia');
+    textSize(25);
 }
 
 //write and declare a draw function
@@ -13,6 +17,6 @@ function draw(){
 
     //write a statement to print the text string 'Hello World'
     //set the location parameters of x=75 and y=75; otherwise with the default settings the text does not show up on our canvas. 
-    text('Hello World!', 75, 75);
+    text('Hello World!', 25, 75);
 
 }
