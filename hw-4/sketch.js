755df8b3f6fdd9b6d41999a59369da2ed36d5328
Julia Ballas <julia.m.ballas@gmail.com>
2018-09-15 08:28:42 -0600

    hw-4 - neck, added triangle

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -11,16 +11,28 @@ function draw() {
   noStroke();
   rect( 200, 200, 400, 400);
   
-  push()
+  push();
     
     fill(50);
     noStroke();
     arc( 60, 400, 400, 600, radians(270), radians(450) );
   
-  pop()
+  pop();
   
+  fill('brown');
+  noStroke();
+  ellipse( 425, 100, 500, 400 );
+
   // neck
   
+  push();
+
+    fill(0);
+    //noStroke();
+    triangle( 500, 300, 350, 600, 600, 600 );
+
+  pop();
+
   // head shape
   
   // face shading, should we try to drop in the light from outside?
