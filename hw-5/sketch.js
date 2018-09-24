d2c19860e59e214a326c911a5e0aba0cdeaec2a3
Julia Ballas <julia.m.ballas@gmail.com>
2018-09-24 16:24:07 -0600

    hw-5: added sunrise, mtns and car
    I added a mountains and a sunrise drifting up and to the left, and instead of a spotlight I have a car driving. Need a road though, a tunnel would be cool too.

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,24 +1,58 @@
  function setup(){
      createCanvas( windowWidth, 600);
 
+}
 
-    
-      }
+
+var sunX = 400;
+var sunY = 400;
 
  function draw(){
 
-    //moved to draw
+    //moved background to draw
     background( 'rgb(5, 4, 255)');
 
     // added variables on the building
     var buildingX = windowWidth / 2;
     var buildingY = windowHeight / 2;
 
-    frameRate(10);
+    frameRate(15);
+
+// sunrise
+    
+push();
+
+sunX = sunX - 1 * 2;
+sunY = sunY - 1 ;
+
+fill( 'green')
+ellipse( sunX, sunY, 75, 75 );
+
+strokeWeight(10);
+stroke('orange');
+fill( 'yellow' );
+ellipse( sunX, sunY, 200, 200 );
+
+pop();
+
 
     // horizon added
-    fill(0)
+    fill(0);
     rect(0, 400, windowWidth, 600);
+
+    // mountains added
+
+    fill(0);
+    triangle( 100, 300, 350, 400, 0, 600 );
+    triangle( 0, 300, 350, 400, 0, 600 );
+    triangle( 440, 300, 500, 400, 0, 600 );
+    triangle( 540, 300, 800, 600, 0, 600 );
+
+    // yard added
+
+    fill( 'brown' );
+    rect(0, 500, windowWidth, 600);
+
 
     // main building
     fill(50);
@@ -42,6 +76,11 @@
 
     pop();
 
-    fill( 'rgba(255, 255, 0, .50)');
-    ellipse( mouseX, mouseY, 50, 50)
- }
+    
+    // spotlight
+    fill( 'rgb(255, 255, 0 )');
+    rect( mouseX, 490, 45, 25, 4);
+    fill(0);
+    ellipse( mouseX + 2, 508, 15, 15, );
+    ellipse( mouseX + 45, 508, 15, 15, );
+    }
