eb8070ed0ee83585118819f465052678be647341
Julia Ballas <julia.m.ballas@gmail.com>
2018-09-24 16:49:45 -0600

    hw-5 Clouds created

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -3,7 +3,6 @@
 
 }
 
-
 var sunX = 400;
 var sunY = 400;
 
@@ -36,6 +35,18 @@ var sunY = 400;
     fill(0);
     rect(0, 400, windowWidth, 600);
 
+    //Clouds controlled by MouseY
+    
+    push();
+        stroke(200);
+        strokeWeight(3);
+        fill(255);
+        ellipse( 4, mouseY, 100, 50);
+        ellipse( 160, mouseY, 220, 110);
+        ellipse( 280, mouseY + 40, 180, 9);
+        ellipse( 20, mouseY - 3, 400, 25);
+    pop();
+
     // mountains added
     fill(0);
     triangle( 100, 300, 350, 400, 0, 600 );
@@ -62,7 +73,7 @@ var sunY = 400;
     fill(50);
     rect( buildingX, buildingY, 200, 500 );
 
-    // adjusting location of window using simple math
+    // adjusting location of windows using simple math
     buildingX = buildingX + 25;
 
     buildingY = buildingY + 25;
@@ -79,7 +90,5 @@ var sunY = 400;
         rect( buildingX , buildingY, 50, 50 );
 
     pop();
-
-    
 
     }
