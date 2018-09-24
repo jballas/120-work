390e3084345d838c71893746fbb5bc41291e930e
Julia Ballas <julia.m.ballas@gmail.com>
2018-09-24 12:46:47 -0600

    updated variable names
    I wanted the Variables to have a more accurate name.

--- course-work/week-5-practice/sketch.js
+++ course-work/week-5-practice/sketch.js
@@ -7,29 +7,29 @@ function setup(){
 
 function draw(){
 
-
-    // create variable for center of triangle connections
+    // create variables for triangle coordinates
     let centerX = 250;
     let centerY = 250;
-    let rightX = 350;
-    let rightY = 150;
-    let leftX = 50;
-    let leftY = 150;
+    let topRightX = 350;
+    let topRightY = 150;
+    let topLeftX = 50;
+    let topLeftY = 150;
     let bottomLeftX = 50;
     let bottomLeftY = 450;
     let bottomRightX = 350;
     let bottomRightY = 400;
 
+    //Four connecting triangles makes a 3D pyramid shape
     strokeWeight(3);
     fill( 'rgba( 255, 0, 130, .60)' );
-    triangle( rightX, rightY, centerX,  centerY, bottomRightX, bottomRightY );
+    triangle( topRightX, topRightY, centerX,  centerY, bottomRightX, bottomRightY );
 
     fill( 'rgb( 0, 0, 130 )' );
-    triangle( leftX, leftY, centerX, centerY, bottomLeftX, bottomLeftY );
+    triangle( topLeftX, topLeftY, centerX, centerY, bottomLeftX, bottomLeftY );
 
     
     fill( 'rgba(5, 255, 0, .60 )' );
-    triangle( leftX, leftY, rightX, rightY, centerX, centerY );
+    triangle( topLeftX, topLeftY, topRightX, topRightY, centerX, centerY );
 
     fill( 'rgba(0, 255, 255, .50 )');
     triangle( centerX, centerY, bottomRightX, bottomRightY, bottomLeftX, bottomLeftY );
