4b61859e3c8ff85a7a26df3ff3ca27673d92af87
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-03 08:45:56 -0600

    hw-6 final edits
    Adjust spacing consistency through code and my comments.

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,6 +1,5 @@
-//global variables
-
-//Javascript object, my triangle variables
+// Global variables
+// Javascript object, my triangle variables
 var tri = {
     topX: 50,
     topY: 50,
@@ -14,7 +13,7 @@ var tri = {
 var moveX = 2;
 var moveY = 2;
 
-// Variable for rotation
+// Variable for pinwheel rotation
 var flip;
 
 // Variables for color
@@ -23,44 +22,44 @@ var green
 var blue
 var opacity
 
-// setup fuction
+// Setup fuction
 function setup() {
-    createCanvas(windowWidth, 400);
+    createCanvas( windowWidth, 400 );
     background(0);
 }
 
-//draw function
+// Draw function
 function draw() {
 
     frameRate(24);
 
-    //Set values to color variables. Using the Absolute Value to keep them from being decimales.
+    // Set values to color variables. Using the Absolute Value to keep them from being decimales.
     red = random( abs(200), abs(255) );
     green = 10;
     blue = random( abs(20), abs(100) );
     opacity = random ( abs(100), abs(255) );
 
-    //move the triangles around using translate and the randomwalker technique, restrict movement with modulo.
+    // Move the triangles around using translate and the randomwalker technique, restrict movement with modulo.
     translate( 100, 100);
-    translate(moveX, moveY);
+    translate( moveX, moveY );
 
-    moveX += random( -moveX, moveX + 1);
-    moveY += random( -moveX, moveX + 1);
+    moveX += random( -moveX, moveX + 1 );
+    moveY += random( -moveX, moveX + 1 );
 
     moveX %= width;
     moveY %= height;
 
     // Map the green value to variable moveX. This way any of the triangles that appear to the left with have more green in them.
-    green = map(moveX, 0, 255, 100, width );
+    green = map( moveX, 0, 255, 100, width );
 
-    //Add rotation value at 90 degrees
+    // Add pinwheel rotation value at 90 degrees
     flip = 90;
 
-    //animate the rotation
+    // Animate the pinwheel rotation
     flip -= random(360);
 
-    //Triangle
-    fill(red, green, blue, opacity);
+    // Triangle
+    fill( red, green, blue, opacity );
     rotate( radians ( flip ) );
     triangle( tri.topX, tri.topX, tri.rightX, tri.rightY, tri.leftX, tri.leftY);
    
@@ -70,6 +69,7 @@ function draw() {
     
     tri.topX %= 100;
 
+    // Since I'm dividing the left position of my triangle, I wanted to make sure it didn't get too small. So I used the floor() function.
     tri.leftX /= random( floor(5) );
     tri.leftY /= random( floor(5) );
 
