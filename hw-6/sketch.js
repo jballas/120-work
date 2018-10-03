7a5dce32d1e4f73dd7c28ea5521baf6b82cc3770
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-03 07:24:13 -0600

    hw-6, Added map function
    I changed the placement from the center to the left side, and added a map function so the triangles on the left would have more green.

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -26,22 +26,22 @@ var opacity
 // setup fuction
 function setup() {
     createCanvas(windowWidth, 400);
-    background(255);
+    background(0);
 }
 
 //draw function
 function draw() {
 
-    frameRate(20);
+    frameRate(24);
 
-    red = random( 200, 255);
-    green = 0;
-    blue = random(20, 100);
+    //Set values to color variables. Using the Absolute Value to keep them from being decimales.
+    red = random( abs(200), abs(255) );
+    green = 10;
+    blue = random( abs(20), abs(100) );
     opacity = random ( abs(100), abs(255) );
 
-    //move the triangles around using translate and the randomwalker technique, restrict movement using modulo.
-    translate( width * .5, height * .5);
-
+    //move the triangles around using translate and the randomwalker technique, restrict movement with modulo.
+    translate( 100, 100);
     translate(moveX, moveY);
 
     moveX += random( -moveX, moveX + 1);
@@ -50,19 +50,21 @@ function draw() {
     moveX %= width;
     moveY %= height;
 
-    //simple rotation 90 degrees
+    // Map the green value to variable moveX. This way any of the triangles that appear to the left with have more green in them.
+    green = map(moveX, 0, 255, 100, width );
+
+    //Add rotation value at 90 degrees
     flip = 90;
 
     //animate the rotation
     flip -= random(360);
 
     //Triangle
-    stroke( 'red' );
     fill(red, green, blue, opacity);
     rotate( radians ( flip ) );
     triangle( tri.topX, tri.topX, tri.rightX, tri.rightY, tri.leftX, tri.leftY);
    
-    // Changed placement of triangle parameters, and limited them using math operations
+    // Changed placement of triangle parameters, and limited them using various math operations
     tri.topX += random();
     tri.topY += random();
     
