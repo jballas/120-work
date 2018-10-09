456a386152b3c82b610d9c76d35eb6f600b54f2f
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-09 14:18:00 -0600

    HW_7 updating final, added images
    Added comments to my final adjustments, and checked to see if I needed the stroke(). Yes, I did.

--- HW-7/sketch.js
+++ HW-7/sketch.js
@@ -20,7 +20,6 @@ function setup() {
 
 function draw() {
 
-
     ball.x += ball.delta_x * ball.scale_x;
     ball.y += ball.delta_y * ball.scale_y;
 
@@ -32,25 +31,40 @@ function draw() {
     }
   
     
-
+    // With a variable in the fill, I can change it
     fill( colBlack );
-    ellipse(ball.x, ball.y, ball.width, ball.width);
+    // New shape, instead of a ball I made it a line.b
+    line(ball.x, ball.y, ball.width, ball.width);
 
+    //If the shape is within certain variables the colors will change to  black, white or gray. Having a stroke makes for interesting patterns.
     if (ball.x >= 100 && ball.x <= 200) {
         stroke(255);
         colBlack = 0;
     } else if ( ball.x >= 200 && ball.x <= 300) {
-        stroke(200);
+        stroke(255);
         colBlack = 100;
     }else {
         stroke(0);
         colBlack = 255;
     }
- 
+
+    // Added instructions for the reset
+    push();
+        textSize(15);
+        stroke(200);
+        fill(100);
+        text('Click any key to reset', 400, 390);
+        text('Click mouse for speed', 200, 390);
+    pop();
+
+    // to reset the background color, press any key
+if (keyIsPressed === true) {
+    background (255);
+}
 
 }
 
 function mousePressed() {
     ball.scale_x = map(mouseX, 0, width, 0.5, 10);
-    ball.scale_y = map(mouseY, 0, height, 0.5, 10);
+    ball.scale_y = map(mouseY, 0, height, .05, 10);
 }
