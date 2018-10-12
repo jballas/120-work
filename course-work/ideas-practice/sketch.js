93e595074855e2d632969a3db71d0cc210a3062b
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-12 10:25:21 -0600

    practicing my for loops
    I tried to make some hills with movement.. doesn't really look like what I wanted.

--- course-work/ideas-practice/sketch.js
+++ course-work/ideas-practice/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas( 800, 800 );
+    createCanvas( windowWidth, 600);
     background(0);
 }
 
@@ -10,6 +10,7 @@ function setup() {
     var centerY = 100;
 
     var d_circle = 50;    
+
 function draw() {
 
     // set up triangle variables
@@ -23,9 +24,27 @@ function draw() {
     var red_Value = color(255, 0, 0);
 
     frameRate(10);
-    noStroke();
+    //noStroke();
 
 
+    // EXPERIMENT with for statement, consecutive hill shapes on the bottom of the screen
+
+
+for(let hill_x = 20; hill_x < width; hill_x += 20) {
+    for (let hill_y = height * .6; hill_y < height - 100; hill_y += 60 ) {
+    fill('#4d2f20');
+    ellipse( hill_x, hill_y, random(ceil(50)), 200 );    
+}   
+    }
+
+
+
+
+
+
+//if( hill_x)
+
+/* EXPERIMENT with if statements, a pear shape into a circle shapes until it grows so big it  set to a simple variable. 
     ellipse(centerX, centerY, d_circle );
 
     centerX ++;
@@ -33,20 +52,31 @@ function draw() {
     d_circle ++;
 
     if( centerY > 200) {
-        centerY = 250;
-        centerX = 250;
+        centerY = width/2;
+        centerX = width/2;
     }
+
+    if(d_circle > width) {
+        d_circle = 500;
+    }
+     //else ( d_circle < 25) {
+    //    d_circle = d_circle ++;
+    //}
 
     push();
         strokeWeight(1);
         stroke(255);
     //   line(500, 500, 800, 800);
+*/
 
+/* EXPERIMENT with four line sinto the corner of the canvas
     for( let c = 500; c < 550; c +=10 ){
         line(c, 500, c+ 300, 800);
     }
 
     pop();
+
+*/
 
 
 /* BELOW IS AN EXPERIMENT WITH TRIANGLES THAT ROTATE
