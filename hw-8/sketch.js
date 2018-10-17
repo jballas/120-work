ac298585a138e8c0df7657d936b8b145f665e5f9
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-16 18:38:51 -0600

    hw-8 consistent code, fixed comments

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -17,8 +17,8 @@ function draw(){
 // Background moved to draw function so it redraws every loop.
 background(0);
 
-//Color variable, because there was some dicussion on the forum and I wanted to use the color() function as a varaible.
-let c_Value = color(255, 200, 0);
+// Color variable, because there was some dicussion on the forum and I wanted to use the color() function as a varaible.
+let c_Value = color( 255, 200, 0 );
 
 frameRate(15);
 
@@ -27,26 +27,27 @@ frameRate(15);
 for (let flower_x = 25; flower_x < width; flower_x ++ ) {
 
     // create a triangle that rotates
-    translate(width * .25, width * .25);
+    translate( width * .25, width * .25 );
     fill( c_Value );
-    rotate ( angle);
-    triangle( flower_x, flower_x + 75, right_x, right_y , left_x, left_y);
+    rotate ( angle );
+    triangle( flower_x, flower_x + 75, right_x, right_y , left_x, left_y );
 }
 
 //Animation, a bouncing effect
-if(left_x >= 50 || left_x < 0 ) {
+if( left_x >= 50 || left_x < 0 ) {
     speed = speed * -1;
 }
 
 left_x = left_x + speed;
 
-print('The value of angle is ' + angle);
+// I needed to visually see if the mousePressed function worked. And I wanted to know which angles to keep in my code. So I printed the value of the angle to the console.
+print( 'The value of angle is ' + angle );
 }
 
-// Click mouse to change the angle. Angle 0 and 1 are not impressive, so I restricted them from 2-15. Because after that the shapes are very similiar to what you've already seen.
+// I used the mousePressed function to change the angle. Angle 0 and 1 are not impressive, so I restricted them from 2-15. The angles after 15 create shapes very similiar to what you've already seen.
 function mousePressed(){
     angle ++;
-    if (angle == 16){
+    if ( angle == 16 ){
         angle = 2;
     }
 }
