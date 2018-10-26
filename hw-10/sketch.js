05c7c76e0513f4cc3df54867cd0840e3e7bbb4cd
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-26 10:28:50 -0600

    hw-10
    still struggling with the for() loop and translate. Although it works, its not really what I want.

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -17,23 +17,27 @@ function draw(){
 push();
 
     // I want to make my flower repeat all over the screen
-    for( let i = 0; i < width; i+= 200){
+    let number_per_row = 20;
+    let flowers = width / number_per_row;
+
+    for( let x_pos = 100; x_pos < width; x_pos += flowers){
+
+        for( let y_pos = 100; y_pos < height; y_pos += flowers){
 
         //I was able to use translate to make my drawFlower function repeat.
-        translate( i, 200);
+        translate( x_pos, y_pos);
         drawFlower();
-    
-
+        }// end of nest for()loop
     } // end of for() loop
 pop();
 
 } // end of draw function
 
 
-function drawFlower(x,y){
+function drawFlower(){
 push();
     scale( .5, .5);
-    fill( 'rgba( 120, 20, 255, .40)' );
+    fill( "rgba( 120, 20, 255, .40)" );
     rotate (mouseX);
     triangle( flower.center_x, flower.center_y, flower.right_x, flower.right_y, flower.left_x, flower.left_y );
 pop();
