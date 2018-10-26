bd471a6518775e78af48c914056a6e081549f253
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-26 09:06:21 -0600

    hw-10 - previous flower sketch
    I created this flower sketch previously, and I love how it makes a chrysanthamum shape with mouseX controlling the petal creation. Plus is colorful and bright. I want to make this into a fuction.

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,21 +1,23 @@
 function setup(){
     createCanvas( windowWidth, 600);
+    background(0);
 }
-
-// flower variables
-flower{
-    center_x: 25,
-    center_y: 100,
-    right_x: 50,
-    right_y: 200
-    left_x: 5,
-    left_y: 200
-
-};
 
 function draw(){
 
+// flower variables
+let flower = {
+    center_x: 25,
+    center_y: 100,
+    right_x: 50,
+    right_y: 200,
+    left_x: 5,
+    left_y: 200
+    };
+
+// flower code from previous sketch. I'm going to take this and make it a function for this week's homework.
 push();
+    translate( 200, 200);
     fill( 'rgba( 120, 20, 255, .40)' );
     rotate (mouseX);
     triangle( flower.center_x, flower.center_y, flower.right_x, flower.right_y, flower.left_x, flower.left_y );
