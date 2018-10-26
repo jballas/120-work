a48a6f7fff4c24c47b6b61552534af50e852b2ef
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-26 09:12:55 -0600

    hw-10, drawFlower function added
    It was easy to change to the sketch into a function called drawFlower(). Too easy. Now I have to try to duplicate it across the screen. I'm going to scale it down first.

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -3,8 +3,6 @@ function setup(){
     background(0);
 }
 
-function draw(){
-
 // flower variables
 let flower = {
     center_x: 25,
@@ -15,13 +13,18 @@ let flower = {
     left_y: 200
     };
 
-// flower code from previous sketch. I'm going to take this and make it a function for this week's homework.
+function draw(){
+
+drawFlower();
+
+}
+
+
+function drawFlower(){
 push();
     translate( 200, 200);
     fill( 'rgba( 120, 20, 255, .40)' );
     rotate (mouseX);
     triangle( flower.center_x, flower.center_y, flower.right_x, flower.right_y, flower.left_x, flower.left_y );
 pop();
-
-
 }
