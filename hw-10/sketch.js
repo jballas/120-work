158408f52d2ca491a2a90c7c64cb99973b6bfd80
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-27 08:40:07 -0600

    hw-10  - added parameters
    I changed my function so it would have parameters. then I used the for() loop with those parameters, but it only made an interesting pattern. It did not get me closer to what I want.
    New screen shot of design.
    
    Perhaps I should try animating my "flower" instead?

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -3,7 +3,7 @@ function setup(){
     background(0);
 }
 
-// flower variables
+/* flower variables
 let flower = {
     center_x: 25,
     center_y: 100,
@@ -13,35 +13,38 @@ let flower = {
     left_y: 200
     };
 
+    */
 function draw(){
+    translate( width * .5, height * .5);
+        
+
 push();
-
     // I want to make my flower repeat all over the screen
-    let number_per_row = 3;
-    let number_per_col = 3;
-    let across = width / number_per_row;
-    let down = width / number_per_col;
+    for( let x_pos = 100; x_pos < width; x_pos += width / 3){
 
-    for( let x_pos = 100; x_pos < width; x_pos += across){
-
-        for( let y_pos = 100; y_pos < height; y_pos += down){
+        for( let y_pos = 100; y_pos < height; y_pos += width / 3){
 
         //I was able to use translate to make my drawFlower function repeat.
-        //Translate( x_pos, y_pos)
         //ellipse( x_pos, y_pos, 50, 50);
-        drawFlower( x_pos, y_pos);
+        drawFlower( x_pos, y_pos, 50, 200, 5, 200);
         }// end of nested for()loop
     } // end of for() loop
+
+
+
 pop();
+
 
 } // end of draw function
 
 
-function drawFlower(x, y){
+function drawFlower( center_x, center_y, right_x, right_y, left_x, left_y){
+
 push();
     scale( .5, .5);
     fill( "rgba( 120, 20, 255, .40)" );
     rotate (mouseX);
-    triangle( flower.center_x, flower.center_y, flower.right_x, flower.right_y, flower.left_x, flower.left_y );
+    //ellipse( center_x, center_y, right_x, right_y);
+    triangle( center_x, center_y, right_x, right_y, left_x, left_y );
 pop();
 }
