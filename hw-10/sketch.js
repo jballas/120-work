fecfdecdc7f1739af844a396891decf5253c6085
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-29 10:49:14 -0600

    hw-10  random petal colors
    I finally got the random petal colors to work when I moved the variable to the global to drawFlower function. Not sure its that proper place for it. I'll try to clean it up. ANyway, it works!
    
    Sorta. I'd prefer to have each 'flower' on the grid to be a different color. Hmm that will be tricky.

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,6 +1,6 @@
 function setup(){
     createCanvas( 600, 600);
-    background(255);
+    background(0);
 }
 
 //flower variables
@@ -15,14 +15,16 @@ let flower = {
 
 
 // Flower array for the colors
-/*let petal_colors = [ "rgba( 120, 20, 255, .40)", "rgba(0, 200, 255, .40)", "rgba(22, 244, 0, .60)", "rgba(195, 2, 255, .40)", "rgba(0, 115, 250, .40)", "rgba(0, 245, 140, .40)" ];
-*/
+let petal_colors = [ "rgba( 120, 20, 255, .40)", "rgba(0, 200, 255, .40)", "rgba(22, 244, 0, .60)", "rgba(195, 2, 255, .40)", "rgba(0, 115, 250, .40)", "rgba(0, 245, 140, .40)" ];
+
 
 
 
 
 
 function draw(){
+
+    frameRate(10)
 
     // I want to make my flower repeat in a grid on the screen
     for( let x_pos = 100; x_pos < 600; x_pos +=200 ){
@@ -34,14 +36,16 @@ function draw(){
         }// end of nested for()loop
     } // end of for() loop
 
-    // create variable with random number from 1-5
-
 } // end of draw function
 
 
 function drawFlower(){
-    let flower_petal = round( random( 50 ));
-    console.log(flower_petal);
+
+    // variable for random petal fill colors
+    let flower_petal = random(petal_colors );
+    
+    //I needed to check if I was getting the random numbers
+    //console.log(flower_petal);
 
 push();
     scale( .5, .5);
