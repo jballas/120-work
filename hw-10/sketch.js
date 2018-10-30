6e19193c3de6f4011be7cca8118095072c413a76
Julia Ballas <julia.m.ballas@gmail.com>
2018-10-30 11:05:38 -0600

    Update hw-10/sketch.js
    changes to commets, spacing to make the code easier to read. I hope

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -14,10 +14,11 @@ let flower = {
     };
 
 // Bee location. I want b_location_x to be 100, 300, or 500. I want b_location_y to be 100, 300, or 500. I think I'll use an array.
+
 // Bee position Array
 let position = [ 100, 300, 500];
 
-  // Bee location variables
+// Bee location variables. It will start in the middle of the screen
 let b_location_x = position[1];
 let b_location_y = position[1];
 
@@ -27,38 +28,42 @@ let petal_angle = 0;
 // Flower array for the colors
 let petal_colors = [ "rgba( 120, 20, 255, .40)", "rgba(0, 200, 255, .40)", "rgba(22, 244, 0, .60)", "rgba(195, 2, 255, .40)", "rgba(0, 115, 250, .40)", "rgba(0, 245, 140, .40)" ];
 
+
 function draw(){
 
     frameRate(10)
 
-
-    // I want to make my flower repeat in a grid on the screen
+    // I want to make my flowers repeat in a grid on the screen
     for( let x_pos = 100; x_pos < 600; x_pos +=200 ){
         for( let y_pos = 100; y_pos < 600; y_pos +=200){
             push();
                 translate(x_pos, y_pos);
                 drawFlower(petal_angle);
             pop();
-        }// end of nested loop
-    } // end loop
+        }
+    }
 
     // Bee function using two arguments for x and y. I'm going to make these variables I can change.
     bee(b_location_x, b_location_y);
 
-    //Petals move authomatically
+    //Petals will move authomatically
     petal_angle ++;
 
-} // end of draw function
+    // text intructions at the bottom
+    textSize(20);
+    fill(255);
+    text(' If you click you might attract more bees', 100, 650);
+}
 
 
-// Function to change the bee's location randomly when you click the mouse.
+// A function to change the bee's location randomly when you click the mouse.
 
 function mousePressed(){
     b_location_x = random (position);
     b_location_y = random (position);
    
-   // Unfortunately I had add the background, otherwise the bee's would become permanent on the sketch.
-    background (0);
+   // Unfortunately I had add the background, otherwise the bee would become permanent bees on the sketch.
+    //background (0);
    }
 
 // Flower function, with one parameter
@@ -97,7 +102,7 @@ function bee(x,y){
     push();
         stroke(0)
         
-        // I made a pattern a stripes using a loop. I had to add in a y loop to make sure the x,y variables could be changed. Math hurts.
+        // I made a pattern of stripes using a loop. I had to add in a y loop to make sure the x,y variables could be changed. Math hurts.
         for(s = x; s < x + 20; s+= 5){
             for ( t = y; t < y + 20; t+=5){
                 line(x - 20, t, x + 15, t);
@@ -105,7 +110,7 @@ function bee(x,y){
         }
     pop();
     
-    // Before I can make a loop, I still have to create all the lines with all the various numbers. Otherwise I don't understand the math. Here the original code for stripes:
+    // Before I can make a loop, I still have to create all the lines with all the various numbers. Otherwise I don't understand the math. Here is the original code for stripes:
 
     /*  line(280, 300, 315, 300);
         line(280, 305, 315, 305);
