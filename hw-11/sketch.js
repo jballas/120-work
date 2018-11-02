e8a17b4303894b537c15202dc8686692625731fe
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-02 10:39:19 -0600

    Update hw-11/sketch.js
    experimenting with the animation of the seed. I can't get it to be jumpy.

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -1,18 +1,27 @@
 function setup() {
     createCanvas(400, 400);
-    background(0);
 }
+
+let jump = 100;
 
 function draw() {
 
+    background(0);
+
+    frameRate(30);
+
     // Seed
     fill('rgb( 255, 150, 20)');
-    ellipse( 100, 100, 15, 30)
+    ellipse( jump, jump, 15, 30)
+
+    jump ++;
+
+    if (jump >= width) {
+        jump = jump * -1;
+    }
 
     //popped corn
-
     fill(255)
-    
     ellipse( 205, 205, 30, 45);
     ellipse(195, 225, 15, 15);
     ellipse(211, 225, 25, 20);
