801828121fcd6e286f5dfd0b107172121483b888
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-02 10:21:48 -0600

    hw-11 added basic designs
    Seed and Popped corn
    Now to figure out how to animate it

---
+++ hw-11/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(400, 400);
+    background(0);
+}
+
+function draw() {
+
+    // Seed
+    fill('rgb( 255, 150, 20)');
+    ellipse( 100, 100, 15, 30)
+
+    //popped corn
+
+    fill(255)
+    
+    ellipse( 205, 205, 30, 45);
+    ellipse(195, 225, 15, 15);
+    ellipse(211, 225, 25, 20);
+    ellipse(220, 215, 18, 18);
+
+}
