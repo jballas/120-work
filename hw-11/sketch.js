0bc90b4dfd9b6f3c5fe5c2d345941eda2e20b344
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-04 19:26:52 -0700

    hw-11 added in movement, functions
    I got some math to work on this, so one seed will bounce randomly. Then I made three functions: seed, poppedCorn, and heatingUp.

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -4,27 +4,52 @@ function setup() {
 
 let jump = 100;
 
+
 function draw() {
 
     background(0);
 
     frameRate(30);
 
-    // Seed
-    fill('rgb( 255, 150, 20)');
-    ellipse( jump, jump, 15, 30)
+seed();
+heatingUp();
 
-    jump ++;
+    
+poppedCorn();
 
-    if (jump >= width) {
-        jump = jump * -1;
+
+}
+
+function seed(){
+// Seed
+        fill('rgb( 255, 150, 20)');
+        ellipse( 100, jump, 15, 30)   
+}
+
+function heatingUp(){
+// random number so it doesn't jump constantly
+let r = ceil(random(1,5));
+
+
+// Seeds need to jump around, as if it's heating up
+    if (r === 2) {
+        jump ++;
+
+        if( jump >= 110){
+            jump = 100;
+        }
+
     }
 
-    //popped corn
-    fill(255)
-    ellipse( 205, 205, 30, 45);
-    ellipse(195, 225, 15, 15);
-    ellipse(211, 225, 25, 20);
-    ellipse(220, 215, 18, 18);
+    console.log(r);
+}
 
+function poppedCorn() {
+    
+    //popped corn
+        fill(255)
+        ellipse( 205, 205, 30, 45);
+        ellipse(195, 225, 15, 15);
+        ellipse(211, 225, 25, 20);
+        ellipse(220, 215, 18, 18);
 }
