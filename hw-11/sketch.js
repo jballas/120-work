a0914c51e89931e76ff1576b59903b1ed7963a36
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-06 17:29:37 -0700

    Revert "Update hw-11/sketch.js"
    This reverts commit ca023b1823e3c9f2daaa2bec99203da13a507f7a.

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -3,7 +3,7 @@ let kernals = 20;
 let bg_color = 0;
 
 function setup(){
-    createCanvas(windowWidth, 400);
+    createCanvas(400, 400);
 
     for ( let i = 0; i < kernals; i++) {
         bucket.push( new Popcorn() );
@@ -36,16 +36,24 @@ class Popcorn {
         this.loc_x = random(width);
         this.loc_y = random(height);
         this.delta = 10;
+<<<<<<< HEAD
         this.popped_x = random(this.loc_x - 3, this.loc_x + 5) ;
         this.popped_y = random(this.loc_y, this.loc_y + 10) ;
 
+=======
+        this.popped_x = random(-3, 5) ;
+        this.popped_y = random(5, 10) ;
+        this.a;
+        this.move_x = 1;
+        this.delta_x = 10;
+>>>>>>> parent of ca023b1... Update hw-11/sketch.js
     }
 
     // I'm using frame to call these methods in the draw function above.
     frame(){
         this.seed();
         this.heatingUp();
-        //this.explodes();
+        this.explodes();
     }
 
     // Display Seed
@@ -55,10 +63,18 @@ class Popcorn {
     }
 
     // Display popped popcorn
+<<<<<<< HEAD
     // How do I make lots of popped corn?
     poppedCornPieces(){
         
         
+=======
+    // How do I make lots of popped corn? Okay, so I figured out I need to have the location_x inside the for()loop as well as in the ellipse parameters. That works now, but its a uniform pattern. how do I make lots of popcorn in random locations?
+    poppedCorn(){
+        
+        for( this.loc_x = this.popped_x; this.loc_x <= width; this.loc_x += 50 ) {
+            for ( this.loc_y = this.popped_y; this.loc_y <= height; this.loc_y += 50 ){
+>>>>>>> parent of ca023b1... Update hw-11/sketch.js
 
                 push();
                     fill( this.poppedCorn_color);
@@ -68,18 +84,21 @@ class Popcorn {
                                 
                     ellipse(this.loc_x, this.loc_y, this.popped_size, this.popped_size);
                                 
+<<<<<<< HEAD
                     ellipse(this.loc_x, this.loc_y, this.popped_size, this.popped_size);
+=======
+                    ellipse(this.loc_x + 4, this.loc_y +3, this.popped_size, this.popped_size);
+>>>>>>> parent of ca023b1... Update hw-11/sketch.js
                 pop();
-
-              
-            //}
-        //}
+            }
+        }
     
     }
 
-    // Seeds need to jump around, as if they are heating up
+    // Seeds need to jump around, as if it's heating up
     heatingUp(){
 
+<<<<<<< HEAD
         if ( this.seed_move <= 2 ) {
            this.seed_y += random(-2, 3);
         } 
@@ -90,11 +109,17 @@ class Popcorn {
             this.poppedCorn();
             }
     
+=======
+        if (this.seed_move <= 2) {
+           this.seed_y += random(-2, 2);
+        }
+>>>>>>> parent of ca023b1... Update hw-11/sketch.js
 
         // checking the randomness of this.seed_move
         console.log(this.seed_move);
     }
 
+<<<<<<< HEAD
     poppedCorn(){
         
         this.loc_x += random(-1, 1);
@@ -104,20 +129,55 @@ class Popcorn {
 
     //TODO: I need to make an explosion method, lines flashing outward from a center point, or something like that.
     // A timed event, after 5 seconds the seeds expand, and "explode". How do I get that?
+=======
+    // A timed event, after 5 seconds the seeds expand, and "explode". 
+>>>>>>> parent of ca023b1... Update hw-11/sketch.js
     explodes(){
-        if( millis() >= 8000 ){
+        if( millis() >= 5000 ){
             this.seed_size_w ++ && this.seed_size_h ++;
             
-                if( this.seed_size_w >= 50){
+                if( this.seed_size_w >= 40){
                     background(bg_color);
+<<<<<<< HEAD
                     for( this.loc_x = 0; this.loc_x <= 200; this.loc_x += 50 ) {
                         for ( this.loc_y = 0; this.loc_y <= 200; this.loc_y += 50 ){
                             this.poppedCorn();
                         }
                     }
+=======
+                    
+                    this.explosion();
+                    this.move();
+                    //this.poppedCorn();
+>>>>>>> parent of ca023b1... Update hw-11/sketch.js
                 }
         }
     }
 
+<<<<<<< HEAD
     
 }
+=======
+    explosion(){
+        
+        for ( this.a = 0; this.a < 50; this.a += 15){
+            push();
+                stroke(255);
+                translate( width * .25, height * .25);
+                rotate(this.a);
+                line(this.move_x * -1, 0, this.move_x, 0);       
+            pop();
+        }
+ 
+    }
+
+    move(){
+
+        this.move_x += this.delta_x;
+    
+        if(this.move_x + this.delta_x >= 100) {
+            this.move_x *= -1;
+        }
+    }
+}
+>>>>>>> parent of ca023b1... Update hw-11/sketch.js
