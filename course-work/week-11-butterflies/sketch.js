3e06768b509adc2537ca6d7848abf509b78814f5
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-09 09:44:33 -0700

    week-11 butterfly experiment
    I made a Class called butterfly, and gave it a flapping motion

---
+++ course-work/week-11-butterflies/sketch.js
@@ -0,0 +1,75 @@
+function setup() {
+
+    createCanvas(windowWidth, 600);
+// This was an experiment in flapping wings
+butterfly = new Butterfly();
+
+}
+
+//let swarm = [];
+//let how_many = 10;
+
+function draw() {
+    background(0);
+    frameRate(8);
+
+    butterfly.display();
+    butterfly.move();
+    
+}
+
+// Butterfly 
+
+class Butterfly {
+
+    constructor(){
+        this.top_x = 5;
+        this.top_y = 50;
+        this.wingTip = 50;
+        this.bottom_x = 5;
+        this.bottom_y = 100;
+        this.flip = 1;
+        this.flap = 1;
+        this.delta = 10;
+
+    }
+
+    frame(){
+        this.display();
+        this.move();
+    }
+
+    //Display Triangle wings
+    display() {
+        translate(width * .25, height * .25);
+
+        triangle(this.top_x, this.top_y, this.wingTip, this.wingTip, this.bottom_x, this.bottom_y);
+
+        push();
+
+            scale( -this.flip , this.flap );
+            
+            stroke(255);
+            noFill();
+            triangle(this.top_x, this.top_y, this.wingTip, this.wingTip, this.bottom_x, this.bottom_y);
+        
+        pop();
+    }
+
+    
+    //Move wings, flapping
+    move() {
+
+        this.flip *= -1;
+        
+        //this.wingTip += this.delta;
+        //this.wingTip % 50;
+
+    }
+    
+
+
+    
+
+}
+
