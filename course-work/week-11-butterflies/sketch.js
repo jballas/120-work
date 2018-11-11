49cd2fc5982450860060bd101e509ce70f1dc4d9
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-11 11:02:42 -0700

    butterflies - update
    edge check is not working

--- course-work/week-11-butterflies/sketch.js
+++ course-work/week-11-butterflies/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
 
-    createCanvas(windowWidth, 600);
-// This was an experiment in flapping wings
+    createCanvas(400, 400);
+// This is an experiment in flapping wings
 butterfly = new Butterfly();
 
 }
@@ -29,42 +29,75 @@ class Butterfly {
         this.wingTip_y = 50;
         this.bottom_x = 5;
         this.bottom_y = 100;
-        this.delta = 10;
+        this.delta_x = 10;
+        this.delta_y = 10
+        this.move_x = width * .25;
+        this.move_y = height * .25;
 
+        this.flip = 1;
+
+        this.r = 3.0;
     }
 
     frame(){
         this.display();
         this.move();
+        this.edgeCheck();
     }
 
     //Display Triangle wings
     display() {
-        translate(width * .25, height * .25);
+        translate(this.move_x, this.move_y);
+
 
         triangle(this.top_x, this.top_y, this.wingTip_x, this.wingTip_y, this.bottom_x, this.bottom_y);
 
         push();
-       
+
+        scale( -this.flip , this.flip );
             stroke(255);
             noFill();
             triangle(this.top_x, this.top_y, this.wingTip_x, this.wingTip_y, this.bottom_x, this.bottom_y);
         
         pop();
+
+        beginShape();
+        vertex(0, -this.r*2);
+        vertex(-this.r, this.r*2);
+        vertex(this.r, this.r*2);
+        endShape(CLOSE);
+    
+        console.log(this.move_x) ;
     }
 
     
     //Move wings, flapping
     move() {
    
-        this.wingTip_x += this.delta;
+
+        this.move_x += this.delta_x;
+        this.move_y += this.delta_y;
+
+        //this.r = 
+       
+        this.wingTip_x += this.delta_x;
         if (this.wingTip_x == 50){
             this.wingTip_x = 0;
-
         }
 
     }
     
+
+    edgeCheck(){
+
+        if (this.move_x <= width ){
+            this.delta_x *= -1;
+        }
+        if(this.move_y >= height) {
+            this.delta_y *= -1;
+        }
+        
+    }
 
 
     
