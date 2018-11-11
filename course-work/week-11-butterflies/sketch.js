5a940e11c4b44fd5b4fd6838848e67a845d767eb
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-11 11:35:55 -0700

    butterfly - final update
    I changed the flapping back. So it goes from 50 to 0.
    
    I aded a body shape in the middle, and removed little triangle.
    
    I adjusted the movement so it almost goes off the top of the screen, like it does on the bottom.

--- course-work/week-11-butterflies/sketch.js
+++ course-work/week-11-butterflies/sketch.js
@@ -26,18 +26,17 @@ class Butterfly {
     constructor(){
         this.top_x = 5;
         this.top_y = 50;
-        this.wingTip_x = 0;
-        this.wingTip_y = 50;
+        this.wingTip_x = 50;
+        this.wingTip_y = 25;
         this.bottom_x = 5;
         this.bottom_y = 100;
         this.delta_x = 10;
         this.delta_y = 10;
-        this.move_x = width * .25;
-        this.move_y = height * .25;
+        this.move_x = random(width);
+        this.move_y = random(height);
 
         this.flip = 1;
         this.flap = 10;
-        this.r = 3.0;
     }
 
     frame(){
@@ -46,27 +45,25 @@ class Butterfly {
         this.edgeCheck();
     }
 
-    //Display Triangle wings
+    //Display butterfly
     display() {
         translate(this.move_x, this.move_y);
 
-
+        // Right wing
         triangle(this.top_x, this.top_y, this.wingTip_x, this.wingTip_y, this.bottom_x, this.bottom_y);
 
+        // Left wing
         push();
 
-        scale( -this.flip , this.flip );
+            scale( -this.flip , this.flip );
             stroke(255);
             noFill();
             triangle(this.top_x, this.top_y, this.wingTip_x, this.wingTip_y, this.bottom_x, this.bottom_y);
         
         pop();
 
-        beginShape();
-        vertex(0, -this.r*2);
-        vertex(-this.r, this.r*2);
-        vertex(this.r, this.r*2);
-        endShape(CLOSE);
+        // Body
+        ellipse( this.top_x - 5 , this.top_y + 20, 10, 50);
     
     }
 
@@ -74,16 +71,15 @@ class Butterfly {
     //Move wings, flapping
     move() {
    
-
+        //Movement across the screen
         this.move_x += this.delta_x;
         this.move_y += this.delta_y;
 
 
-        //this.r = 
-       
-        this.wingTip_x += this.flap;
-        if (this.wingTip_x == 50){
-            this.wingTip_x = 0;
+        // Wing's flapping       
+        this.wingTip_x -= this.flap;
+        if (this.wingTip_x == 0){
+            this.wingTip_x = 50;
         }
 
         console.log( this.move_x);
@@ -96,7 +92,7 @@ class Butterfly {
         if (this.move_x >= width | this.move_x <= 0 ){
             this.delta_x *= -1;
         }
-        if(this.move_y >= height || this.move_y <= 0 ) {
+        if(this.move_y >= height || this.move_y <= -100 ) {
             this.delta_y *= -1;
         }
         
