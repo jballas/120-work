a2fba59ebb80e6c9ccef6c4b41a48ee5c28315ac
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-12 12:08:21 -0700

    I tried putting in a class
    I'm just getting further away from my catterpillars and the class just made it confusing. What am I trying to make here?

--- course-work/week-11-caterpillar/sketch.js
+++ course-work/week-11-caterpillar/sketch.js
@@ -1,14 +1,14 @@
+let circleSize = [];
+let how_many  = 20;
+
 function setup(){
     createCanvas( windowWidth, 600);
+ 
+    for( i = 0; i < how_many; i += 50){
+        circleSize.push (new Spinner() );
+    }
 
 }
-
-
-let jump = 500;
-let speed = 10;
-
-let leftWall = 50;
-let rightWall = 75;
 
 
 function draw(){
@@ -16,22 +16,37 @@ function draw(){
     frameRate(2);
 
     translate( width/4, height/4);
+
     let spring = ceil( random(40, 50) );
-    
 
-    for( i = 0; i < jump; i += spring){
-        ellipse(i, width/8, 50 );    
-    }
-
-      
-    //if (spring >= width || spring <= 0){
-    //    spring *= -1; 
-    //}
+for (let i = 0; i < circleSize.length; i++){
+    circleSize[i].frame( 50, 50, spring)
+}
 
     console.log(spring);
 
-    //jump += speed ;
-    //if( jump >= 120 || jump <= 0) {
-    //    jump = 5
-    //}
+}
+
+
+class Spinner {
+
+    constructor(){
+        this.size = ceil( random(50,400) );
+        this.jump = 500;
+        
+    }
+
+    frame(x,y,size){
+        this.display(x,y,size);
+
+    }
+
+    display(x,y, size) {
+
+        stroke(255);
+        noFill(); 
+        ellipse(x,y, size );
+    
+    }
+
 }
