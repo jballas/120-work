633ebf8c457a331185bb1d512aaa5a329a014965
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-12 12:15:15 -0700

    week-11 - circles
    it almost looks like ripples but again. Not a caterpillar. I'm going to go back to the beginning. All new code.

--- course-work/week-11-caterpillar/sketch.js
+++ course-work/week-11-caterpillar/sketch.js
@@ -1,45 +1,37 @@
-let circleSize = [];
-let how_many  = 20;
 
 function setup(){
     createCanvas( windowWidth, 600);
  
-    for( i = 0; i < how_many; i += 50){
-        circleSize.push (new Spinner() );
-    }
-
+    spinner = new Spinner();
+    
 }
 
 
 function draw(){
-    background(0);  
     frameRate(2);
 
+    background(0);  
     translate( width/4, height/4);
 
     let spring = ceil( random(40, 50) );
+    let size = ceil( random(50,120) );
 
-for (let i = 0; i < circleSize.length; i++){
-    circleSize[i].frame( 50, 50, spring)
-}
+    
+    spinner.display( spring, 50, size);
+
+
 
     console.log(spring);
 
 }
 
-
 class Spinner {
 
     constructor(){
-        this.size = ceil( random(50,400) );
         this.jump = 500;
         
     }
 
-    frame(x,y,size){
-        this.display(x,y,size);
-
-    }
 
     display(x,y, size) {
 
