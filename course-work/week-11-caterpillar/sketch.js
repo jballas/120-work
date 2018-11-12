d20cb3b1a6888ff0758b471ddbc1adfa9d5eed81
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-12 13:16:23 -0700

    week 11
    Welp. Nothing is showing up. I have no idea why. I can't find any errors.

--- course-work/week-11-caterpillar/sketch.js
+++ course-work/week-11-caterpillar/sketch.js
@@ -1,23 +1,21 @@
+let bugs = [];
+let how_many_bugs = 10;
 
-let wiggle = [];
-let xPos;
 function setup(){
     createCanvas( windowWidth, 600);
  
     background(0);  
 
-    for (let i = 0; i < 10; i ++ ){
-        wiggle.push ( ceil( random(40, 55)) )
+    for (let i = 0; i < how_many_bugs; i ++ ){
+        bugs.push ( new Caterpillar() );
     }
 
-    console.log(wiggle);
 }
 
 
 function draw(){
     frameRate(2);
 
-    translate( width/4, height/4);
 
 
     //xPos = ceil( random(40, 50));
@@ -26,8 +24,35 @@ function draw(){
        // ellipse(i, xPos, 50);
     //}
 
-    for( i = 0; i < wiggle.length; i ++){
-        ellipse(i* 50, wiggle[i], 50);
+    for(let  i = 0; i < bugs.length; i ++){
+        bugs[i].frame();
+    }
+
+
+}
+
+
+class Caterpillar {
+
+    construction(){
+        this.x_location = 50;
+        this.y_location = ceil( random(40, 55) );
+        this.size = 50;
+        this.color = 255;
+    }
+
+    frame() {
+        this.display();
+    }
+    
+    display() {
+
+        push();
+        
+            translate( width * .25, height* .25);
+            fill(this.color);
+            ellipse(this.x_location, this.y_location, this.size);
+        pop();
     }
 
 
