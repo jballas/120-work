8dad876111e407d64aacdfcb6892595a888b4976
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-13 08:37:52 -0700

    Revert "week-11 - circles"
    This reverts commit 633ebf8c457a331185bb1d512aaa5a329a014965.

--- course-work/week-11-caterpillar/sketch.js
+++ course-work/week-11-caterpillar/sketch.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let bugs = [];
 let how_many_bugs = 30;
 
@@ -11,6 +12,50 @@ function setup(){
 
 }
 
+=======
+let circleSize = [];
+let how_many  = 20;
+
+function setup(){
+    createCanvas( windowWidth, 600);
+ 
+    for( i = 0; i < how_many; i += 50){
+        circleSize.push (new Spinner() );
+    }
+
+}
+
+
+function draw(){
+    background(0);  
+    frameRate(2);
+
+    translate( width/4, height/4);
+
+    let spring = ceil( random(40, 50) );
+
+for (let i = 0; i < circleSize.length; i++){
+    circleSize[i].frame( 50, 50, spring)
+}
+
+    console.log(spring);
+
+}
+
+
+class Spinner {
+
+    constructor(){
+        this.size = ceil( random(50,400) );
+        this.jump = 500;
+        
+    }
+
+    frame(x,y,size){
+        this.display(x,y,size);
+
+    }
+>>>>>>> parent of 633ebf8... week-11 - circles
 
 function draw(){
     frameRate(20);
