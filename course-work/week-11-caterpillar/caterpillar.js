6f6f7f379cc459960efe7a5964d1ddee54566420
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-13 09:13:51 -0700

    Added comments, and bounce
    Comments to identify what does what.
    
    I divided the movement function into forward momentum and the bouncing effect. Just to clarify it.

--- course-work/week-11-caterpillar/caterpillar.js
+++ course-work/week-11-caterpillar/caterpillar.js
@@ -1,9 +1,11 @@
+// I was attempting to make caterpillars, but it looks nothing like that. It's more like a 3D cylinder, or a waterfall. Anyway, I'm leaving the name. 
 class Caterpillar {
 
     constructor(){
         this.x_location = 50;
         this.y_location = ceil( random(40, 55) );
         this.size = 50;
+        this.diamter = this.size/2;
         this.delta_x = 10;
         this.delta_y = random(10);
     }
@@ -12,8 +14,10 @@ class Caterpillar {
     frame() {
         this.display();
         this.move();
+        this.bounce();
     }
     
+    // Display the screen.
     display() {
 
         push();
@@ -28,22 +32,35 @@ class Caterpillar {
 
     }
 
+    // This makes the circles move across the screen
     move() {
 
+        // Horizontal movement
         this.x_location += this.delta_x;
+
+        // Vertical movement
+        this.y_location += this.delta_y;
+
+    }
+
+    bounce(){
 
         if (this.x_location >= width || this.x_location <= 0){
             this.delta_x *= -1;
         }
 
-      this.y_location += this.delta_y;
-
         if (this.y_location >= height -100 || this.y_location <= 0){
             this.delta_y *= -1;
         }
-
     }
 
+    // New goal. Make things interact, somehow.
+
+    //interact(){
+
+        //if (this.diameter)
+
+    //}
 
 
 }
