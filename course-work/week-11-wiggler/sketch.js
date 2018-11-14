933a9c37a64aa6ea8700dfba5219a95429ac77b1
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-14 08:55:16 -0700

    week-11 wiggler experiment

---
+++ course-work/week-11-wiggler/sketch.js
@@ -0,0 +1,27 @@
+let wiggle = [];
+let xPos = 0;
+
+function setup(){
+    createCanvas( windowWidth, 600);
+
+    background(0);  
+}
+
+
+function draw(){
+    frameRate(20);
+
+    translate( width/4, height/4);
+    let spring = ceil( random(40, 50) );
+    
+    wiggle.push ( ceil( random(40, 50)) );
+
+    for (let i = 0; i < wiggle.length; i +=10 ){
+        ellipse(xPos, wiggle[i], 50);
+    }
+
+    xPos = (xPos + 10) % windowHeight;
+
+    console.log(wiggle);
+
+}
