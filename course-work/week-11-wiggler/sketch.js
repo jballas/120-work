6ffa39e16e1917155b8db00fff6fd230022f1a67
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-14 12:25:57 -0700

    week-11 update
    I just realized I named all these files week 11 and I'm actually working on week 12.
    ANYWAY, It wiggles how I wanted, but the vertical movement is not working at the moment.

--- course-work/week-11-wiggler/sketch.js
+++ course-work/week-11-wiggler/sketch.js
@@ -4,10 +4,11 @@ let wiggler = [];
 function setup(){
     createCanvas( 600, 600);
 
+   
     for (let i = 0; i < 10; i ++ ){
-        net.push ( ceil( random(40, 50)) )
+        net.push( ceil( random(40, 50)) )
+    
         wiggler.push(new Wiggler());
-
     }
 
     console.log(net);
@@ -36,6 +37,7 @@ class Wiggler {
         this.random_x = 0;
         this.random_y = 0;
 
+
     }
 
     frame(index){
@@ -51,14 +53,16 @@ class Wiggler {
             translate( this.random_x, this.random_y);
 
             fill(this.bodyColor );
-            ellipse(i* 50 + this.xPos, index + this.yPos, 50);
-
+            
+            //for (let i = 0; i < 10; i ++ ){
+                ellipse(i * 50 + this.xPos, index + this.yPos, 50);
+            //}
         pop();
     }
 
     //sideways and vertical movement
     move(){
-        this.xPos ++;
+        this.xPos +=10;
         this.yPos = random(-2, 2) ;
     }
 
@@ -67,8 +71,10 @@ class Wiggler {
     nextAppears(){
         if (this.xPos >= width){
             this.xPos = -500;
-            //this.random_y = ceil( random(height));
+           //this.random_y = ceil( random(height));
         }
+
+        //console.log(this.random_y);
     }
 
 }
