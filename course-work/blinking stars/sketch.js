d93c8516aa84a479027252d1e56f6a3696d4ecfd
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-15 07:40:21 -0700

    blinking star update
    removed old code I wasn't using anymore. Cleaned up code.

--- course-work/blinking stars/sketch.js
+++ course-work/blinking stars/sketch.js
@@ -23,37 +23,6 @@ star( width * .25, height * .25);
 star(width * .75, height * .75);
 move();
 
-//REPEATING LINES, flashing effect.
-/*
-for ( a = 0; a < 50; a += random(15)){
-    push();
-        translate( width * .25, height * .25);
-        rotate(a);
-        line(loc_x * -1, loc_y, loc_x, loc_y);       
-    pop();
-}
-*/
-
-
-//POINT
-push();
-    strokeWeight(5);
-    point(250, 250);
-pop();
-
-
-
-//console.log( z_loc);
-
- /*my loops never work until I understand what I'm trying to repeat.
-
-for(x = 0; x < width; x += 50){
-    for (y = 0; y < 500; y += 50){
-        line(y, x, x-25, y-25 );
-
-    }
-*/
-
 }
 
 function star(x, y){
@@ -70,7 +39,7 @@ function star(x, y){
 
 function move(){
 
-    //I change from + to minus and not it bounces. It's very much like twinking stars.
+    //I change from + to minus and not it bounces. It's very much like twinking stars. Althought they blink at the same time. Too bad I can't add an element of randomness to their blinking.
     loc_x -= delta_x;
 
     if(loc_x + delta_x >= 0 || loc_x + delta_x <= 100) {
