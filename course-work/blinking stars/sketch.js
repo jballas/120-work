fa8c87940accb0e8d5c4ecd9f7bacfa9ebbc79d6
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-15 07:47:59 -0700

    blinking stars - update - final
    I'm moving on to another experiement. I was looking for a popping effect, but I'm happy how thit is blinking stars.
    
    I'm leaving the frame rate at 10, so it doesn't get too slow. 3 is too slow.

--- course-work/blinking stars/sketch.js
+++ course-work/blinking stars/sketch.js
@@ -10,15 +10,17 @@ function setup() {
 // Nice, I changed the initial location of X and Y from 0 to 10 and it make a scattered, dropping.
 // removing the Move function and making location of X to 1000 gets me a long scattered lines
 let loc_x = 25;
-
-// When I changed y to 10, they became actual star shapes!!! let loc_y = 10;
+//At 0 they are just lines that blink. let loc_y = 0;
+// At 20 they are a polygon with extra lines sticking out. let loc_y = 20;
+// When I changed y to 10, they became actual star shapes!!!
+let loc_y = 10;
 let delta_x = 10;
 
 
 function draw(){
 
     background( r,g,b );
-    frameRate(3);
+    frameRate(10);
 
 star( width * .25, height * .25);
 star(width * .75, height * .75);
@@ -40,7 +42,7 @@ function star(x, y){
 
 function move(){
 
-    //I change from + to minus and not it bounces. It's very much like twinking stars. Althought they blink at the same time. Too bad I can't add an element of randomness to their blinking.
+    //I change from + to minus and now it bounces. It's very much like twinking stars. Althought they blink at the same time. Too bad I can't add an element of randomness to their blinking.
     loc_x -= delta_x;
 
     if(loc_x + delta_x >= 0 || loc_x + delta_x <= 100) {
