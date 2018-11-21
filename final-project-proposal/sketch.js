4f586b330ed1f21fbdc2940e732e1565205e8ead
Julia Ballas <julia.m.ballas@gmail.com>
2018-11-21 13:32:42 -0700

    created new class called Goal
    Now to figure out how to make these two objects interact.

--- final-project-proposal/sketch.js
+++ final-project-proposal/sketch.js
@@ -3,16 +3,36 @@ let player_init_y = 50;
 let player;
 
 function setup() {
-  createCanvas(400, 400);
-  player = new Player(player_init_x, player_init_y);
-    
+    createCanvas(400, 400);
+    player = new Player(player_init_x, player_init_y);
+    goal = new Goal();
 }
 
 function draw() {
   background(0);
 	
 
+    goal.displayPortal();
+
     player.display();
     player.move();
     player.inBounds();
+
+    
+}
+
+
+class Goal {
+
+    constructor(){
+        this.size = width * .25;
+        this.x = width * .5;
+        this.y = height * .5;
+    }
+
+    displayPortal(){
+
+        ellipse(this.x, this.y, this.size);
+    }
+    
 }
