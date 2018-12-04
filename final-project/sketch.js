02f009d2093fc362709bf06b218b8b3da4d0a400
Julia Ballas <julia.m.ballas@gmail.com>
2018-12-04 15:51:59 -0700

    remove the html
    I took out the html in index. I was putting the canvas in a strange posistion. I will try a button though.

--- final-project/sketch.js
+++ final-project/sketch.js
@@ -43,22 +43,26 @@ function preload() {
 }
 
 function setup() {
+    //let canvas = 
     createCanvas(1024, 576);
-
+    //canvas.position(0,0);
 
     //Play the game's background music
     game_bg_music.setVolume(0.5);
     game_bg_music.play();
 
     
+    // Player's Y starting position is a random location
     player_init_y = abs( random(60, width/2) );
-    
+
+
     player[0] = new Player(player_init_x, player_init_y, lillia_img);
 
     goal = new Goal();
 
 
     for (let i = 0; i < howManyFairies; i++){
+        // Enemy position is random, but in the lower quadrant of the screen
         enemy_init_y = abs ( random(height/2, height) );
         enemy_init_x = abs ( random ( width / 2, width) );
 
@@ -68,23 +72,26 @@ function setup() {
 
 function draw() {
 
-
     background( bg_img );
         
-    let target = createVector( player[0].pos.x, player[0].pos.y); // This creates the vector target that the enemy will seek. I didn't need an array to hold the pos.x,pos.x. I needed a vector.
+    let target = createVector( player[0].pos.x, player[0].pos.y); // This creates the vector target that the enemy will seek. I didn't need an array to hold the pos.x, pos.x. I needed a vector.
 
     goal.displayPortal();
 
+    // Create player character
     for(let i = 0; i < player.length; i ++){
         player[i].frame();
     }
+
+    //If you reach the goal you win.
     reachedGoal();
 
+    // Enemy characters
     for (let i = 0; i < fairies.length; i++) {
         fairies[i].frame(target);
         fairies[i].avoidOthers(fairies, i);
     }
-   // gameOver();
+   // gameOver(); // I moved gameOver into the enemy.js file.
 }
 
 function reachedGoal(){
@@ -98,6 +105,8 @@ function reachedGoal(){
 
         //Play the 'You win' Sound Effect
         game_clear.play();
+
+        createP("Thank you for playing my game!");
 
         stroke(255)
         textSize(50);
